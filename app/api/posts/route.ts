995a982386e84d17b01a2ca0cdf60bab61254ffa
tereshonkov <tereshonkov.dima@gmail.com>
2025-03-13 17:59:12 +0200

    route

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,20 +1,28 @@
 import { NextResponse } from "next/server";
+import data from "./data";
 
 export async function GET(req: Request) {
-    return NextResponse.json({ message: "Hello, World!" }); //Enter your JSON data here
+    return NextResponse.json(data); //Enter your JSON data here
 }
 
 export async function POST(req: Request) {
     const body = await req.json();
-    return NextResponse.json({ message: `Hello, ${body.name}!` }); //Edit your JSON data here
+    return NextResponse.json(data.push(body)); //Edit your JSON data here
 }
 
-export async function DELETE(req: Request) {
-    const { id } = await req.json();
-    return NextResponse.json({ message: `Deleted ${id}` }); //Edit your JSON data here
-}
+// export async function DELETE(req: Request) {
+//     const { id } = await req.json();
+//     return NextResponse.json({ message: `Deleted ${id}` }); //Edit your JSON data here
+// }
 
 export async function PUT(req: Request) {
-    const data = await req.json();
-    return NextResponse.json({ message: `Updated ${data.id}` }); //Edit your JSON data here
+    const updatedPost = await req.json();
+    const index = data.findIndex(post => post.id === updatedPost.id);
+
+    if (index !== -1) {
+        data[index] = { ...data[index], ...updatedPost }; // Оновлення тільки переданими полями
+        return NextResponse.json({ message: "Post updated", updatedPost: data[index] });
+    } else {
+        return NextResponse.json({ error: "Post not found" }, { status: 404 });
+    }
 }
