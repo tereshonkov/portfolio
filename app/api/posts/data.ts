9d1d1c0a970c02fa606502fa4c193b4a708b93fe
tereshonkov <tereshonkov.dima@gmail.com>
2025-03-13 17:49:15 +0200

    Form Comments

---
+++ app/api/posts/data.ts
@@ -0,0 +1,15 @@
+interface Post {
+    title: string;
+    content: string;
+    id: number;
+}
+
+const data: Post[] = [
+    {
+        title: "First Post",
+        content: "This is the first post on the blog!",
+        id: 1,
+    },
+];
+
+export default data;
