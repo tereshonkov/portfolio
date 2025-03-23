7d1d6f0d057303d89a7461ea961be991cfa59fc8
tereshonkov <tereshonkov.dima@gmail.com>
2025-03-23 13:49:34 -0700

    end edit today

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -26,11 +26,11 @@ export default function Modal({ id, closeModal, array }: ModalProps) {
                         <Image src={el.image} width={300} height={300} alt="decstop"/>
                         {el.imageTwo && <Image src={el.imageTwo} width={100} height={100} alt="mobile"/>}
                     </div>
-                    <div className="flex flex-col gap-5 w-[90vw] md:w-[50%] h-[30vh] justify-end md:justify-center p-2">
+                    <div className="flex flex-col gap-2 w-[90vw] md:w-[50%] h-[30vh] justify-end md:justify-center p-1">
                         <button onClick={closeModal} className="absolute top-5 right-5 text-3xl text-gray-500 cursor-pointer">X</button>
                         <h1 className="text-gray-500 text-x md:text-4xl">{el.title}</h1>
                         <p className="text-gray-500 text-xs md:text-l">{el.description}</p>
-                        <a href={el.url} className="text-gray-500 text-l md:text-l">{el.url}</a>
+                        <a href={el.url} className="w-[70%] text-gray-500 text-xs md:text-l">{el.url}</a>
                     </div>
                 </div>
                 </div>
