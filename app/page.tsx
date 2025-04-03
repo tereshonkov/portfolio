8f7aa3844f0a6defb208a1c14190848f36599b25
tereshonkov <tereshonkov.dima@gmail.com>
2025-04-03 13:49:34 +0300

    edited page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
 // Code: portfolio/app/page.tsx
-import Portfolio from "@/components/Portfolio/Portfolio";
-import Header from "../components/Header/Header";
-import Hero from "@/components/Hero/Hero";
-import About from "@/components/About/About";
-import Stack from "@/components/Stack/Stack";
 import Contact from "@/components/Contact/Contact";
 import Footer from "@/components/Footer/Footer";
 import Header2 from "@/components/Header2/Header2";
